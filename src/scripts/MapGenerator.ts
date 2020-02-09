97623f9324819894035892fd7b7012efedfc2453
Eoin Flanagan <outterest@hotmail.com>
2020-02-09 15:31:28 +0100

    Updated map generator to produce a real map

--- src/scripts/MapGenerator.ts
+++ src/scripts/MapGenerator.ts
@@ -1,6 +1,8 @@
 import { Tile } from "./objects/Tile";
 import { GameStateData } from "./objects/GameState";
 import { River } from "./objects/River";
+import { Mountain } from "./objects/Mountain";
+import { indexToMapCoordinates } from "./widgets/utils";
 
 export interface MapGeneratorConfig {
     numTilesX: number;
@@ -14,14 +16,19 @@ const DEFAULT_MAP_CONFIG: MapGeneratorConfig = {
 
 export class MapGenerator {
     rnd: Phaser.Math.RandomDataGenerator
-    constructor(seed: number) {
-        this.rnd = new Phaser.Math.RandomDataGenerator(seed.toString());
+    constructor(seed?: number) {
+        this.rnd = new Phaser.Math.RandomDataGenerator(seed ? seed.toString(): undefined);
     }
 
     public generateNewMap({
         numTilesX,
         numTilesY
     }: MapGeneratorConfig = DEFAULT_MAP_CONFIG): GameStateData {
+        let mountains = this.generateMountains(numTilesX, numTilesY);
+        const rivers = this.generateRivers(numTilesX, numTilesY);
+        // Prevent overlap of mountains and rivers
+        mountains = mountains.filter(mountain => rivers.every(river => river.x !== mountain.x || river.y !== mountain.y));
+
         return {
             numTilesX,
             numTilesY,
@@ -31,11 +38,17 @@ export class MapGenerator {
                     growth: 2,
                     type: "1",
                     x: 13,
-                    y: 0,
+                    y: 2,
+                },
+                "1": {
+                    growth: 2,
+                    type: "2",
+                    x: 2,
+                    y: 13,
                 }
             },
-            mountains: [],
-            rivers: this.generateRivers(numTilesX, numTilesY),
+            mountains,
+            rivers,
             players: {
                 "1": {
                     controlledBy: "Human",
@@ -43,26 +56,49 @@ export class MapGenerator {
                     cloudOwned: null,
                     flowers: ["0"],
                     seedsOwned: ["1"]
+                },
+                "2": {
+                    controlledBy: "AI_2",
+                    autoReplantTileMap: {},
+                    cloudOwned: null,
+                    flowers: ["1"],
+                    seedsOwned: ["2"]
                 }
             },
             flowerTypes: {
                 "1": {
-                    name: "Test flower",
+                    name: "Tulip",
                     nitrogen: 2,
                     potassium: 2,
-                    phosphorous: 2,
-                    seedProductionRate: 99,
-                    tenacity: 50,
-                    turnsUntilDead: 3,
+                    phosphorous: 1,
+                    seedProductionRate: 35,
+                    tenacity: 0,
+                    turnsUntilDead: 4,
                     turnsUntilGrown: 3,
                     type: "1"
+                },
+                "2": {
+                    name: "Elderflower",
+                    nitrogen: 2,
+                    potassium: 2,
+                    phosphorous: 1,
+                    seedProductionRate: 35,
+                    tenacity: 0,
+                    turnsUntilDead: 4,
+                    turnsUntilGrown: 3,
+                    type: "2"
                 }
             },
             seedStatus: {
                 "1": {
                     progress: 0,
-                    quantity: 100,
+                    quantity: 5,
                     type: "1"
+                },
+                "2": {
+                    progress: 0,
+                    quantity: 5,
+                    type: "2"
                 }
             },
             flowerAugmentations: {},
@@ -72,32 +108,30 @@ export class MapGenerator {
     }
     
     private generateTiles(numTilesX: number, numTilesY: number): Tile[] {
-        let n = 1;
-        let p = 1;
-        let k = 1;
+        const centrePoint = {x: (numTilesX - 1) / 2, y: (numTilesY - 1) / 2 };
+        const radius = (numTilesX / 2) + (numTilesY / 2) / 2;
+
+        const nBreakpoints = [3, 3, 3, 2, 2, 2, 2, 1, 1, 1];
+        const pBreakpoints = [2, 2, 2, 2, 2, 1, 1, 1, 1, 1];
+        const kBreakpoints = [2, 1, 1, 1, 2, 2, 2, 2, 2, 2]
         return new Array<Tile | undefined>(numTilesX * numTilesY)
             .fill(undefined)
             .map((_, index) => new Tile(index))
             .map((tile) => {
+                const {x, y} = indexToMapCoordinates(tile.index, numTilesX);
+                const distanceFromCentre = Phaser.Math.Distance.Between(centrePoint.x, centrePoint.y, x, y);
+                
+                const ratio = (distanceFromCentre / radius);
+
                 tile.soil = {
-                    nitrogenContent: n,
-                    potassiumContent: p,
-                    phosphorousContent: k
-                }
-                n++;
-                if (n > 3) {
-                    n = 1;
-                    p++;
-                    if (p > 3) {
-                        k++;
-                        p = 1;
-                        if (k > 3) {
-                            k = 1;
-                        }
-                    }
+                    nitrogenContent: nBreakpoints[Math.floor(ratio * nBreakpoints.length)],
+                    phosphorousContent: pBreakpoints[Math.floor(ratio * pBreakpoints.length)],
+                    potassiumContent: kBreakpoints[Math.floor(ratio * kBreakpoints.length)]
                 }
                 return tile;
             });
+
+        
     }
 
     private generateRivers(numTilesX: number, numTilesY: number): River[] {
@@ -107,6 +141,31 @@ export class MapGenerator {
                 x: index % numTilesX,
                 y: Math.floor(index / numTilesX)
             }))
-            .filter(() => this.rnd.integerInRange(0, 3) == 2);
+            .filter(() => this.rnd.integerInRange(1, 18) == 1)
+            .reduce<River[]>((mirroredRivers, river) => { 
+                // Mirror mountains to both sides for balance
+                return [
+                    ...mirroredRivers,
+                    {x: river.x, y: river.y},
+                    {x: numTilesX - river.x - 1, y: numTilesY - river.y - 1}
+                ];
+            }, []);
+    }
+    private generateMountains(numTilesX: number, numTilesY: number): Mountain[] {
+        return new Array<River | undefined>(numTilesX * numTilesY)
+            .fill(undefined)
+            .map((_, index) => ({
+                x: index % numTilesX,
+                y: Math.floor(index / numTilesX)
+            }))
+            .filter(() => this.rnd.integerInRange(1, 17) == 1)
+            .reduce<Mountain[]>((mirroredMountains, mountain) => { 
+                // Mirror mountains to both sides for balance
+                return [
+                    ...mirroredMountains,
+                    {x: mountain.x, y: mountain.y},
+                    {x: numTilesX - mountain.x - 1, y: numTilesY - mountain.y - 1}
+                ];
+            }, []);
     }
 }
